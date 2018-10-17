35841c1f2582fefd63558bfdd21104097de90a8e
Anwesh43 <anweshthecool0@gmail.com>
2018-10-17 16:25:50 +0530

    adding drawSISNode canvas extension method

--- signalindicatorstepview/src/main/java/ui/anwesome/com/signalindicatorstepview/SignalIndicatorStepView.kt
+++ signalindicatorstepview/src/main/java/ui/anwesome/com/signalindicatorstepview/SignalIndicatorStepView.kt
@@ -15,6 +15,29 @@ val nodes : Int = 5
 
 val factor : Int = 4
 
+fun Canvas.drawSISNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val kGap : Float = (2 * gap / 3) / factor
+    val sFactor : Float = 1f / factor
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * i + gap, h/2)
+    for (j in 0..factor-1) {
+        val sc : Float = Math.min(sFactor, Math.max(0f,scale - sFactor * j)) *  factor
+        save()
+        translate(kGap * j, 0f)
+        paint.color = Color.parseColor("#9E9E9E")
+        drawLine(0f, 0f, 0f, -kGap, paint)
+        paint.color = Color.parseColor("#3F51B5")
+        drawLine(0f, 0f, 0f, -kGap * sc, paint)
+        restore()
+    }
+    restore()
+}
+
 class SignalIndicatorStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
