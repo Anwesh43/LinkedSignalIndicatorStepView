dcd877bb56314c3731305f961f508c547f87321c
Anwesh43 <anweshthecool0@gmail.com>
2018-10-17 18:04:32 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/linkedsignalstepindicatorview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/linkedsignalstepindicatorview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.linkedsignalstepindicatorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.signalindicatorstepview.SignalIndicatorStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SignalIndicatorStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
