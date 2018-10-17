634f40cceb2ddc1595f6f93448c5361f53d33ad9
Anwesh43 <anweshthecool0@gmail.com>
2018-10-17 16:16:52 +0530

    adding State class

--- signalindicatorstepview/src/main/java/ui/anwesome/com/signalindicatorstepview/SignalIndicatorStepView.kt
+++ signalindicatorstepview/src/main/java/ui/anwesome/com/signalindicatorstepview/SignalIndicatorStepView.kt
@@ -13,6 +13,8 @@ import android.content.Context
 
 val nodes : Int = 5
 
+val factor : Int = 4
+
 class SignalIndicatorStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -28,5 +30,23 @@ class SignalIndicatorStepView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
+        fun update(cb : (Float) -> Unit) {
+            scale += 0.05f * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+            }
+        }
     }
 }
