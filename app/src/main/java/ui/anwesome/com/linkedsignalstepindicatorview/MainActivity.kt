e5fb662507ff42d9c2add2138d097c2d06e9d8dc
Anwesh43 <anweshthecool0@gmail.com>
2018-10-17 17:08:49 +0530

    adding view in activity

--- app/src/main/java/ui/anwesome/com/linkedsignalstepindicatorview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/linkedsignalstepindicatorview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.linkedsignalstepindicatorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.signalindicatorstepview.SignalIndicatorStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SignalIndicatorStepView.create(this)
     }
 }
