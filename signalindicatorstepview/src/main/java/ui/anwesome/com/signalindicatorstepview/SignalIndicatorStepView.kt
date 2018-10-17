0f7075c2bb32bb21d97228e3d8d4e2a7890f533c
Anwesh43 <anweshthecool0@gmail.com>
2018-10-17 16:29:19 +0530

    adding SISNode class

--- signalindicatorstepview/src/main/java/ui/anwesome/com/signalindicatorstepview/SignalIndicatorStepView.kt
+++ signalindicatorstepview/src/main/java/ui/anwesome/com/signalindicatorstepview/SignalIndicatorStepView.kt
@@ -99,4 +99,47 @@ class SignalIndicatorStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SISNode(var i : Int, val state : State = State()) {
+        private var next : SISNode? = null
+        private var prev : SISNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SISNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSISNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SISNode {
+            var curr : SISNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
